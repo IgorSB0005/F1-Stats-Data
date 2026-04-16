5f15431c088b2deb7c739a7bc6a9ee39da56c370
SviatoslavJ <fwsviatoslav@gmail.com>
2026-04-16 19:07:14 +0200

    change old cards designe in dashboard

--- frontend/components/dashboard.tsx
+++ frontend/components/dashboard.tsx
@@ -5,14 +5,14 @@ export default function Dashboard() {
   return (
     <div className="relative min-h-[calc(100vh-80px)] overflow-hidden">
       <Image
-        src="/anotherPic/f1GrandPrix.jpg"
+        src="/anotherPic/dashBackground.jpg"
         alt=""
         fill
         className="object-cover -z-10"
         priority
       />
       <div className="mx-auto max-w-2xl px-6 lg:max-w-7xl lg:px-8">
-        <p className="mx-auto mt-45 mb-30 max-w-lg text-center text-4xl font-semibold tracking-tight text-balance text-white sm:text-5xl">
+        <p className="mx-auto mt-45 mb-30 max-w-lg text-center text-4xl font-semibold tracking-tight text-balance text-red-600 sm:text-5xl">
           Everything you need about favorite sport
         </p>
         <div className="mt-10 grid gap-4 sm:mt-16 lg:grid-cols-3 lg:grid-rows-2">
@@ -20,7 +20,7 @@ export default function Dashboard() {
             <div className="absolute inset-px rounded-lg bg-black/20 backdrop-blur-sm lg:rounded-l-4xl" />
             <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] lg:rounded-l-[calc(2rem+1px)]">
               <div className="px-8 pt-8 pb-3 sm:px-10 sm:pt-10 sm:pb-0">
-                <p className="mt-2 text-lg font-medium tracking-tight text-white max-lg:text-center">
+                <p className="mt-2 text-lg font-medium tracking-tight text-red-600 max-lg:text-center">
                   DRIVERS STANDINGS
                 </p>
               </div>
@@ -38,57 +38,43 @@ export default function Dashboard() {
           </div>
           <div className="relative max-lg:row-start-1">
             <div className="absolute inset-px rounded-lg bg-black/20 backdrop-blur-sm max-lg:rounded-t-4xl" />
+
             <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)]">
-              <div className="px-8 pt-8 sm:px-10 sm:pt-10">
-                <p className="mt-2 text-lg font-medium tracking-tight text-white max-lg:text-center">
-                  STATS BATTLE
-                </p>
-                <p className="mt-2 mb-10 max-w-lg text-sm/6 text-gray-400 max-lg:text-center">
-                  Enjoy the ability to quickly and easily compare drivers or
-                  teams using statistical indicators
-                </p>
-              </div>
-              <div className="@container relative w-full max-lg:max-w-xs h-[300px] overflow-hidden">
+              <div className="@container relative w-full aspect-[4/5] overflow-hidden">
                 <Image
                   alt=""
-                  src="/dashboardPictures/dashboardBattle.png"
+                  src="/dashboardPictures/dashSBcard.png"
                   fill
-                  className="object-cover object-center"
+                  className="object-cover object-center scale-110"
                 />
                 <div className="absolute inset-0 bg-gradient-to-t from-black/60 to-transparent" />
               </div>
             </div>
+
             <div className="pointer-events-none absolute inset-px rounded-lg shadow-sm outline outline-white/15 max-lg:rounded-t-4xl" />
           </div>
           <div className="relative max-lg:row-start-3 lg:col-start-2 lg:row-start-2">
             <div className="absolute inset-px rounded-lg bg-black/20 backdrop-blur-sm" />
+
             <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)]">
-              <div className="px-8 pt-8 sm:px-10 sm:pt-10">
-                <p className="mt-2 text-lg font-medium tracking-tight text-white max-lg:text-center">
-                  HISTORY
-                </p>
-                <p className="mt-2 mb-10 max-w-lg text-sm/6 text-gray-400 max-lg:text-center">
-                  Learn the history of the royal races, as well as the most
-                  interesting facts about your favorite sport
-                </p>
-              </div>
-              <div className="@container relative w-full max-lg:max-w-xs h-[300px] overflow-hidden">
+              <div className="@container relative w-full aspect-[3/4] overflow-hidden">
                 <Image
                   alt=""
-                  src="/dashboardPictures/dashboardHistory.jpg"
+                  src="/dashboardPictures/dashboardCard.png"
                   fill
-                  className="object-cover object-center"
+                  className="object-contain object-center scale-110"
                 />
                 <div className="absolute inset-0 bg-gradient-to-t from-black/60 to-transparent" />
               </div>
             </div>
+
             <div className="pointer-events-none absolute inset-px rounded-lg shadow-sm outline outline-white/15" />
           </div>
           <div className="relative lg:row-span-2">
             <div className="absolute inset-px rounded-lg bg-black/20 backdrop-blur-sm max-lg:rounded-b-4xl lg:rounded-r-4xl" />
             <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] max-lg:rounded-b-[calc(2rem+1px)] lg:rounded-r-[calc(2rem+1px)]">
               <div className="px-8 pt-8 pb-3 sm:px-10 sm:pt-10 sm:pb-0">
-                <p className="mt-2 mb-10 text-lg font-medium tracking-tight text-white max-lg:text-center">
+                <p className="mt-2 mb-10 text-lg font-medium tracking-tight text-red-600 max-lg:text-center">
                   F1 NEWS
                 </p>
                 <NewsSection />
